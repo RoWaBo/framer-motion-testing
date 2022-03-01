4ef39a023063a509d39c38a7259531a66c6a3ef1
Robert <u1501851@rts-365.dk>
2022-03-01 08:49:46 +0100

    Created Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import LayoutTest from "./pages/LayoutTest";
+import Login from "./pages/Login";
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<LayoutTest />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</>
 	);
